1359556454dc50620612b350209f67634c766c0b
Jayanth9710 <kumarjayanth55@gmail.com>
2021-12-21 23:41:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const dotenv = require("dotenv")
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+const path = require('path');
+
+app.use(cors({
+    origin: "*"
+}))
+
+app.use(express.json());
+
+app.post("/newfile", async function(req,res) {
+try {
+    const date = new Date;
+    const currentDate=`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    const currentTime= `${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+    
+    
+    const timestamp = Date.now();
+
+   fs.mkdir('Text folder', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.writeFile(`./Text folder/${currentDate}-${currentTime}.txt`, new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(timestamp), (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Successfully created file.');
+                }
+            });
+        }
+    })
+    res.json({
+        message:"File Created"
+    })
+} catch (error) {
+    console.log(error)
+}
+})
+
+app.get("/folder",async function (req,res) {
+    try {
+        fs.readdir(`./Text folder`,{withFileTypes:true},(err,data)=>{
+            if(err){
+                // return res.json(err)
+                console.log(err)
+            }
+            const result = []; 
+            for (const entry of data) {
+                result.push({name: entry.name, type: entry.isDirectory() ? "folder" : "file", ext: path.extname(entry.name)});
+            }
+         res.json(result);
+        })
+    } catch (error) {
+        res.json(`status:failed & ${error}`);
+    }
+   
+})
+
+app.listen(PORT, function () {
+    console.log(`The app is listening in port ${PORT}`)
+})
